6c3a0606131e479939096ba4338d5fbe19fc5d74
Raghu Gopalakrishnan <graghunair@gmail.com>
2020-08-23 15:27:05 +0300

    Update DDL.sql

--- DBA_LOCAL/DDL.sql
+++ DBA_LOCAL/DDL.sql
@@ -1,22 +1,7 @@
 SET NOCOUNT ON
 GO
 
-USE [dba_local]
-GO
-
-DROP TABLE IF EXISTS [dbo].[tblDBMon_Config_Details]
-GO
-CREATE TABLE [dbo].[tblDBMon_Config_Details](
-					[Config_Parameter] [varchar](100) NOT NULL,
-					[Config_Parameter_Value] [varchar](400) NOT NULL,
-					[Updated_By] [nvarchar](256) NOT NULL,
-					[Date_Updated] [datetime] NOT NULL) 
-GO	
-ALTER TABLE [dbo].[tblDBMon_Config_Details] ADD CONSTRAINT [PK_tblDBMon_Config_Details_Config_Parameter] PRIMARY KEY ([Config_Parameter])
-ALTER TABLE [dbo].[tblDBMon_Config_Details] ADD CONSTRAINT [DF_tblDBMon_Config_Details_Updated_By] DEFAULT (suser_sname()) FOR [Updated_By]
-ALTER TABLE [dbo].[tblDBMon_Config_Details] ADD CONSTRAINT [DF_tblDBMon_Config_Details_Date_Updated] DEFAULT (getdate()) FOR [Date_Updated]
-GO
-INSERT INTO [dbo].[tblDBMon_Config_Details]([Config_Parameter], [Config_Parameter_Value]) VALUES ('Purge_tblDBMon_SQL_Server_Threshold', '10')
+USE [DBA_DBMon]
 GO
 
 DROP TABLE IF EXISTS [dbo].[tblDBMon_SP_Version] 
@@ -30,14 +15,17 @@ GO
 ALTER TABLE [dbo].[tblDBMon_SP_Version] ADD CONSTRAINT [PK_tblDBMon_SP_Version] PRIMARY KEY ([SP_Name])
 GO
 
-DROP TABLE IF EXISTS [dbo].[tblDBMon_SQL_Server]
+DROP TABLE IF EXISTS [dbo].[tblDBMon_SQL_Servers]
 GO
-CREATE TABLE [dbo].[tblDBMon_SQL_Server](
-	[Date_Captured] DATETIME NOT NULL,
+CREATE TABLE [dbo].[tblDBMon_SQL_Servers](
+--Inventory
 	[Server_Name] NVARCHAR(128) NOT NULL,
+	[Handshake_Timestamp] DATETIME NULL,
 	[Domain] VARCHAR(50) NULL,
 	[IP_Address] VARCHAR(50) NULL,
 	[Port] INT NULL,
+	[Is_Active] BIT NOT NULL,
+	[Is_Production] BIT NOT NULL,
 	[Server_Host] nvarchar(128) NULL,
 	[Edition] nvarchar(128) NULL,
 	[Product_Version] nvarchar(128) NULL,
@@ -46,11 +34,17 @@ CREATE TABLE [dbo].[tblDBMon_SQL_Server](
 	[User_Databases] SMALLINT NULL,
 	[AOAG_Health] BIT NULL,
 	[AOAG_Details] XML NULL,
+	[Application_Category] VARCHAR(500) NULL,
+	[Application_Contact] VARCHAR(500) NULL,
+	[Comments] VARCHAR(2000) NULL,
+--H/W Specs
 	[CPU] INT NULL,
 	[Physical_Memory_KB] BIGINT NULL,
 	[Committed_Target_KB] BIGINT NULL,
+--Configuration
 	[SQL_Memory_Model] NVARCHAR(120) NULL,
-	[Server_Services] XML NULL,
+	[Server_Services] XML,
+--Monitoring
 	[SQLServer_Start_Time] DATETIME NULL,	
 	[Full_Backup_Timestamp] XML NULL,
 	[TLog_Backup_Timestamp] XML NULL,
@@ -60,7 +54,65 @@ CREATE TABLE [dbo].[tblDBMon_SQL_Server](
 	[TLog_Utlization] XML,
 	[Errors_And_Warnings] XML NULL,
 	[File_System_Space] XML NULL,
-	[Script_Version] XML NULL)
+	[Script_Version] XML NULL,
+	[Date_Entered] DATETIME NOT NULL
+)
+
+ALTER TABLE [dbo].[tblDBMon_SQL_Servers] ADD CONSTRAINT [PK_tblDBMon_SQL_Servers_Server_Name] PRIMARY KEY CLUSTERED ([Server_Name]);
+ALTER TABLE [dbo].[tblDBMon_SQL_Servers] ADD CONSTRAINT [DF_tblDBMon_SQL_Servers_Date_Entered] DEFAULT GETDATE() FOR [Date_Entered] ;
+ALTER TABLE [dbo].[tblDBMon_SQL_Servers] ADD CONSTRAINT [DF_tblDBMon_SQL_Servers_Is_Active] DEFAULT 1 FOR [Is_Active] ;
+ALTER TABLE [dbo].[tblDBMon_SQL_Servers] ADD CONSTRAINT [DF_tblDBMon_SQL_Servers_Production] DEFAULT 1 FOR [Is_Production] ;
 GO
-CREATE CLUSTERED INDEX [IDX_tblDBMon_SQL_Server_Date_Captured] ON [dbo].[tblDBMon_SQL_Server]([Date_Captured])
+
+INSERT INTO [dbo].[tblDBMon_SQL_Servers]([Server_Name]) VALUES ('server-0')
+INSERT INTO [dbo].[tblDBMon_SQL_Servers]([Server_Name]) VALUES ('server-1')
+GO
+SELECT * FROM [dbo].[tblDBMon_SQL_Servers]
+GO
+
+DROP VIEW IF EXISTS [dbo].[vwDBMon_SQL_Servers]
+GO
+
+CREATE VIEW [dbo].[vwDBMon_SQL_Servers]
+AS
+SELECT	[Server_Name],
+		[Handshake_Timestamp],
+		DATEDIFF(mi, [Handshake_Timestamp], GETDATE()) AS [Last_Handshake_Mins],
+		[Domain],
+		[IP_Address],
+		[Port],
+		[Is_Active],
+		[Is_Production],
+		[Server_Host],
+		[Edition],
+		[Product_Version],
+		[Is_Clustered],
+		[Is_Hadr_Enabled],
+		[User_Databases],
+		[AOAG_Health],
+		[AOAG_Details],
+		[Application_Category],
+		[Application_Contact],
+		[Comments],
+		[CPU],
+		[Physical_Memory_KB],
+		[Committed_Target_KB],
+		[SQL_Memory_Model],
+		[Server_Services],
+		[SQLServer_Start_Time],
+		[Full_Backup_Timestamp],
+		[TLog_Backup_Timestamp],
+		[Blocking],
+		[CPU_Utilization],
+		[Page_Life_Expectancy],
+		[TLog_Utlization],
+		[Errors_And_Warnings],
+		[File_System_Space],
+		[Script_Version],
+		[Date_Entered]
+FROM [dbo].[tblDBMon_SQL_Servers]
+GO
+
+SELECT	*
+FROM	[dbo].[vwDBMon_SQL_Servers]
 GO
